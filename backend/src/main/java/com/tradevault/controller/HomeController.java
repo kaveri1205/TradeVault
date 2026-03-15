9cbfb09386874e3fe3ebac0199142f81fa9f6c9d
Kaver <23eg105v09@anurag.edu.in>
2026-03-15 12:26:16 +0530

    fix1

---
+++ backend/src/main/java/com/tradevault/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.tradevault.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "TradeVault API is running successfully 🚀";
+    }
+}
